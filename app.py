1da56c3aa42bc49b95d5c87db086339cd1dda476
jxu093 <jxu093@gmail.com>
2015-11-07 15:50:25 -0500

    Get tweets
    almost done

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
+import twitter
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return redirect('/app')
 
 
-@app.route("/formsubmit")
-def form_submit():
+@app.route("/app", methods=['POST', 'GET'])
+def main():
     if request.method == 'POST':
-        pass
+        topic = request.form['searchtopic']
+        results = twitter.twit_search(topic)
+
+        return render_template('home.html', result=results)
+
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
--- twitter.py
+++ twitter.py
@@ -10,7 +10,7 @@ oauth_consumer_secret="mBlreU1Uuj0AdFV40q2y1Cha7w1PsrMi7cB2wlzQajsakn0mWe"
 oauth_access_token="254556588-NaI5yKZCQJhlvlXQKgNRfquQnjpKAokKNNAUuoQq"
 oauth_access_secret="mkQZpH1hH3BpbtZFe3V1u9y7D4lEOWyKawRpwjJB5qURr"
 
-tweet_list = []
+
 def get_oauth():
     oauth = OAuth1(oauth_consumer_key,
                 client_secret=oauth_consumer_secret,
@@ -18,17 +18,22 @@ def get_oauth():
                 resource_owner_secret=oauth_access_secret)
     return oauth
 
+
 def twit_search(query):
     url = 'https://api.twitter.com/1.1/search/tweets.json?q='+query
     oauth = get_oauth()
-    json_obj = requests.get(url,auth=oauth)
+    json_obj = requests.get(url, params={'count':100}, auth=oauth)
     data = json_obj.json()
+
+    tweet_list = []
 
     for item in data['statuses']:
         tweet_list.append(item['text'])
 
-    for i in range(0, len(tweet_list)):
-        print '\n' + tweet_list[i]
+    # for i in range(0, len(tweet_list)):
+    #     print '\n' + tweet_list[i]
+
+    return tweet_list
 
 
-twit_search('toronto')
+# twit_search('toronto')
